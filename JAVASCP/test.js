cb95ef36d9b4f6df89b90739cfef5119b38b027d
BangJems <ryanafifi77@student.ub.ac.id>
2026-06-14 21:09:59 +0700

    test1

---
+++ JAVASCP/test.js
@@ -0,0 +1,70 @@
+// console.log("bagaimana kabarmu ? ");
+
+
+// BELAJAR JAVASCRIPT
+
+// let nama = "jamal";
+// let umur = "23";
+
+// console.log("mana kamu adalah " + nama , "dan umur kamu adalah " + umur); 
+
+// \\ BELAJAR JAVASCRIPT 3 
+
+// let nama = "subar"; 
+// const umur = "23";
+// var kota = "jakarta";
+
+// console.log("Nama kamu adalah " + nama , "dan umur kamu adalah " + umur , "kamu lahir di " + kota ); 
+
+//  BELAJAR JAVASCRIPT 4
+
+// let saldo = 1000000;
+// saldo = saldo + 1000000;
+
+// console.log("saldo anda adalah " + saldo );
+
+// let saldo = 100;
+
+// if (saldo > 100) {
+//     console.log("saldo anda cukup untuk membeli barang ini");
+// } else {
+//     console.log("saldo anda tidak cukup untuk membeli barang ini");
+// }
+
+
+// const jam = 10;
+// if (jam === 12 || jam === 11.50) {
+//     console.log("anda terlambat mengumpulkan tugas ");
+// } else if (jam === 11) {
+//     console.log("anda hampir terlambat mengumpulkan tugas ");
+// } else {
+//     console.log("anda tepat waktu mengumpulkan tugas ");
+// }
+
+// Belajar loop 
+
+// for (let i = 0; i < 5; i++ ){
+//     console.log(" perulangan ke " + i );
+// }
+
+for (let i = 5; i >= 1; i-- ){
+    let bintang = "";
+
+    for (let j = 1; j <= i; j++){
+        bintang = bintang + "*";
+    }
+
+    console.log(bintang);
+
+}   
+
+
+for (let i = 5; i >= 1; i--) {
+  let bintang = "";
+
+  for (let j = 1; j <= i; j++) {
+    bintang = bintang + "*";
+  }
+
+  console.log(bintang);
+}
